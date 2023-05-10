aaac49ce976b349bd00da8f4ed52d13ee8a230a5
Tess Hauck <shimmybo@gmail.com>
2023-05-10 10:35:05 -0700

    adding getting user info and skill info per user

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,25 @@ const knexConfig = require("../knexfile");
 const knex = require("knex")(knexConfig["development"]);
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("success");
-});
+router
+  //get info for specific user
+  .get("/:userId", (req, res) => {
+    knex("users")
+      .where("id", req.params.userId)
+      .select("*")
+      .then((result) => {
+        res.status(200);
+        res.send(result);
+      });
+  })
+  //get skills for specific user
+  .get("/skills/:userId", (req, res) => {
+    knex("skills")
+      .where("user_id", req.params.userId)
+      .select("*")
+      .then((result) => {
+        res.send(result);
+      });
+  });
 
 module.exports = router;
