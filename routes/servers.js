65769e083e76486ca8a963cb800022d97b0e3978
Tess Hauck <shimmybo@gmail.com>
2023-05-11 17:16:15 -0700

    checks on searching server

--- routes/servers.js
+++ routes/servers.js
@@ -7,59 +7,87 @@ const puppeteer = require("puppeteer");
 const { v4: uuidv4 } = require("uuid");
 const { authorize } = require("../utils");
 const jwt = require("jsonwebtoken");
+const cheerio = require("cheerio");
 
 router
-  .post("/", (req, res) => {
+  .post("/", async (req, res) => {
     id = uuidv4();
-    const imgPath = `/public/images/${id}.png`;
+    const imgPath = `/images/${id}.png`;
     const url = req.body.url;
+    const exists = await knex("servers").where({ url }).first();
     //axios call to get page info
-    axios.get(url).then((response) => {
-      const level = Math.round(response.data.length / 1000);
-      const health = level * 10;
 
-      //inserting into database
-      knex("servers")
-        .whereNot("url", url)
-        .insert({
-          id: id,
-          user_id: null,
-          url: url,
-          image_url: imgPath,
-          server_level: level,
-          health: health,
-        })
-        .then((result) => {
-          //gets screenshot of page
-          puppeteer
-            .launch({
-              defaultViewport: {
-                width: 1280,
-                height: 1024,
-              },
+    if (!exists) {
+      axios
+        .get(url)
+        .then((response) => {
+          const level = Math.round(response.data.length / 1000);
+          const health = level * 10;
+          const html = cheerio.load(response.data);
+          const title = html("title").text();
+          //inserting into database
+          knex("servers")
+            .insert({
+              id: id,
+              user_id: null,
+              url: url,
+              image_url: imgPath,
+              server_level: level,
+              health: health,
+              server_name: title,
             })
-            .then(async (browser) => {
-              const page = await browser.newPage();
-              await page.goto(url);
-              await page.screenshot({ path: `.${imgPath}` });
-              await browser.close();
+            .then(async (result) => {
+              //gets screenshot of page
+              puppeteer
+                .launch({
+                  defaultViewport: {
+                    width: 1280,
+                    height: 1024,
+                  },
+                })
+                .then(async (browser) => {
+                  const page = await browser.newPage();
+                  await page.goto(url);
+                  await page.screenshot({ path: `./public${imgPath}` });
+                  await browser.close();
+                });
+              const server = await knex("servers").where({ id }).first();
+              res.status(200).send(server);
+            })
+            .catch((err) => {
+              res.status(err.response.status).send(err);
             });
-          res.status(200).send("Successful search");
         })
         .catch((err) => {
-          res.send(err);
+          res.status(err.response.status).send(err);
         });
-    });
+    } else {
+      res.status(200).send(exists);
+    }
   })
   //getting servers based on a user id
   .get("/", async (req, res) => {
-    console.log(req.headers.authorization);
     decodedToken = authorize(req.headers.authorization);
     knex("servers")
       .where("user_id", decodedToken.id)
       .select("*")
       .then((query) => {
         res.send(query);
+      });
+  })
+  .get("/:serverId", (req, res) => {
+    const id = req.params.serverId;
+    knex("servers")
+      .where({ id })
+      .then((result) => {
+        if (result.length > 0) {
+          res.status(200).send({ exists: true });
+        } else {
+          res.status(404).send({ exists: false });
+        }
+      })
+      .catch((err) => {
+        res.status(404).send(err);
       });
   })
   //updating server with userid
